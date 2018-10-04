fc2519cd0bc25791b19da165f2c3e20874a42cb1
jordanmceachern <jormceachern@gmail.com>
2018-10-04 12:11:14 -0400

    added some google oauth flow

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {console.log(accessToken)})
+)
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile','email']
+})
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
