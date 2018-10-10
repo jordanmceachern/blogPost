1018aba1cbb1da24c88ac1c0ddd470fba0c61d00
jordanmceachern <jormceachern@gmail.com>
2018-10-09 22:01:16 -0400

    changed client json name to client

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') { //heroku automatically sets this environment variable to 'production' in prod mode
     module.exports = require('./prod')
 } else {
     module.exports = require('./dev')
