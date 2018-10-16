2009c152a83ab99d35e824e6b527d5d6eb3dac64
jordanmceachern <jormceachern@gmail.com>
2018-10-16 13:39:52 -0400

    git ignore is now working

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '1044878284110-9uj96ut6c83smrvrb82auoj9pqev9rgo.apps.googleusercontent.com',
-    googleClientSecret: 'cvz_TS8h8BH6CsZHUp1ZjWg0',
-    mongoURI: 'mongodb://jormceachern:newbeginnings91!@ds125912.mlab.com:25912/blog--post',
-    cookieKey: '159webdevelopment278isthebest347neverstopneverstopping471'
-}
